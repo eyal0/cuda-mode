2ebf9b0f44e296e8724b14be4717efb852b75f0f
eyal0 <109809+eyal0@users.noreply.github.com>
2022-04-26 19:44:59 -0600

    Run c mode hooks for cuda, too.

--- cuda-mode.el
+++ cuda-mode.el
@@ -344,6 +344,7 @@ Key bindings:
   (run-hooks 'cuda-mode-hook)
   (setq font-lock-keywords-case-fold-search t)
   (setq c-buffer-is-cc-mode 'c++-mode)
+  (c-run-mode-hooks 'c-mode-common-hook)
   (c-update-modeline))
 
 (provide 'cuda-mode)
